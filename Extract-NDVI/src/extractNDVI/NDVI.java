0cb803370a347f187015acf4c029628e2c7f3c45
Daniel Hernandez <dhdan97@gmail.com>
2019-11-22 00:03:57 -0500

    working on moveToDirectory

--- Extract-NDVI/src/extractNDVI/NDVI.java
+++ Extract-NDVI/src/extractNDVI/NDVI.java
@@ -1,9 +1,31 @@
 package extractNDVI;
 
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.commons.io.FileUtils;
+
 public class NDVI {
 
 	public NDVI() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean isValid(String fileName) {
+		if(fileName.contains(".QKM.VI_NDVI.005.") && !k.getFileName().toString().contains(".jpg")) {
+			return true;
+		}
+	}
+	
+	public void moveToDirectory(DirectoryStream<Path> directory) {
+		DirectoryStream<Path> directoryStream1 = Files.newDirectoryStream(i);//added .getFileName()
+		for(Path k : directoryStream1) {
+			if(k.getFileName().toString().contains(".QKM.VI_NDVI.005.") && !k.getFileName().toString().contains(".jpg")) {
+				System.out.println(k);
+				FileUtils.copyFileToDirectory(k.toFile(), dst.toFile());
+			}
+		}
 	}
 
 }
