34bdcdab519de557ba47ba05be7eaa6c69daa846
Daniel Hernandez <dhdan97@gmail.com>
2019-11-22 01:13:17 -0500

    finished renameFiles

--- Extract-NDVI/src/extractNDVI/ExtractDriver.java
+++ Extract-NDVI/src/extractNDVI/ExtractDriver.java
@@ -16,16 +16,15 @@ public class ExtractDriver {
 	public static void main(String[] args) throws IOException {
 		
 		Scanner input = new Scanner(System.in);
-
 		//Acquire directory; better not to hard code this since I will be creating my own sample directories
-		String hpumaPath = "C:\\Users\\pumah\\Desktop\\Src_Folder";
-		String hpumaDst = "C:\\Users\\pumah\\Desktop\\Dump_Folder";
-		String dhdanPath = "F:\\FellowshipWork-Daniel.h\\NDVI Data\\2000\\RenamedData";
-
 		System.out.println("Please enter directory:");
-		Path path = Paths.get(hpumaPath);
+		Path path = Paths.get(input.nextLine());
 		input.close();
-		Path dst = Paths.get(hpumaDst).toAbsolutePath();
+		
+		//have Henry insert his own path and change the string
+		String dhdanPath = "F:\\FellowshipWork-Daniel.h\\NDVI Data\\2000\\RenamedData";
+		String hpumaPath;
+		Path dst = Paths.get(dhdanPath).toAbsolutePath();
 		
 		//if valid extraction directory and valid output directory
 		if(Files.exists(path) && Files.isDirectory(path) && Files.exists(dst) && Files.isDirectory(dst)) {
@@ -35,14 +34,15 @@ public class ExtractDriver {
 			DirectoryStream<Path> mainStream = Files.newDirectoryStream(path);
 			for(Path i : mainStream) {
 				System.out.println(i.getFileName());
-				NDVI.moveToDirectory(i, dst);
+//				NDVI.moveToDirectory(i, dst);
 				
-				//for every file inside the extraction directory
-//				DirectoryStream<Path> innerStream = Files.newDirectoryStream(i);
+//				for every file inside the output directory
+//				DirectoryStream<Path> innerStream = Files.newDirectoryStream(dst);
 //				for(Path j : innerStream) {
 //					System.out.println("	" + j.getFileName());
 //				}
 			}
+			NDVI.renameFiles(dst);
 		}
 	}
 
--- Extract-NDVI/src/extractNDVI/NDVI.java
+++ Extract-NDVI/src/extractNDVI/NDVI.java
@@ -1,5 +1,6 @@
 package extractNDVI;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -18,7 +19,44 @@ public class NDVI {
 //			return true;
 //		}
 //	}
-	
+	public static void renameFiles(Path outputFolder) throws IOException {
+		DirectoryStream<Path> directoryStream = Files.newDirectoryStream(outputFolder);
+		
+		//for every file in output folder
+		for(Path p : directoryStream) {
+			
+			//if not already renamed(ie filename does not contain "US", get FileInfo
+			if(p.getFileName().toString().contains("US")) {
+				FileInfo n1 = new FileInfo(p.getFileName().toString());
+				System.out.println("Data Range: Days " + n1.getStartDay() + "-" + n1.getEndDay());
+				System.out.println("Year: " + n1.getYear());
+				
+				//if .met file
+				if(p.getFileName().toString().contains(".met")) {
+					File f1 = new File(p.toString().replace(p.getFileName().toString(), "") + n1.getYear() + "0" + (n1.getStartDay()/7) + ".met");
+					p.toFile().renameTo(f1);
+				}
+				//if .tif.xml
+				if(p.getFileName().toString().contains(".tif.xml")) {
+					File f1 = new File(p.toString().replace(p.getFileName().toString(), "") + n1.getYear() + "0" + (n1.getStartDay()/7) + ".tif.xml");
+					p.toFile().renameTo(f1);
+				}
+				//if .tif
+				if(p.getFileName().toString().contains(".tif") && !p.getFileName().toString().contains(".xml") && !p.getFileName().toString().contains(".ovr")) {
+					File f1 = new File(p.toString().replace(p.getFileName().toString(), "") + n1.getYear() + "0" + (n1.getStartDay()/7) + ".tif");
+					p.toFile().renameTo(f1);
+				}
+				if(p.getFileName().toString().contains(".tif.aux.xml")) {
+					File f1 = new File(p.toString().replace(p.getFileName().toString(), "") + n1.getYear() + "0" + (n1.getStartDay()/7)+ ".tif.aux.xml");
+					p.toFile().renameTo(f1);
+				}
+				if(p.getFileName().toString().contains(".tif.ovr") && p.getFileName().toString().contains("US")) {
+					File f1 = new File(p.toString().replace(p.getFileName().toString(), "") + n1.getYear() + "0" + (n1.getStartDay()/7)+ ".tif.ovr");
+					p.toFile().renameTo(f1);
+				}	
+			}
+		}
+	}
 	//accepts a src path(single folder) and copies proper .tif file to dst directory
 	public static void moveToDirectory(Path src, Path dst) throws IOException {
 		DirectoryStream<Path> directoryStream1 = Files.newDirectoryStream(src);
